8b1a1f6ad0c1d4f23613243fd1aca54c392f5021
KateKampf <katyakampf@gmail.com>
2018-07-02 17:04:47 +0300

    add bottom navigation

--- app/src/main/java/com/example/kampf/games/MainActivity.java
+++ app/src/main/java/com/example/kampf/games/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.kampf.games;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == bottomNavigationView.getSelectedItemId()) {
+
+            return true;
+
+        }
+
+        String message = "";
+
+
+        switch (item.getItemId()) {
+
+            case R.id.nav_games:
+
+                message = "Games";
+                break;
+
+            case R.id.nav_companies:
+
+                message = "Companies";
+                break;
+
+            case R.id.nav_favorite:
+
+                message = "Favorite";
+                break;
+
+            case R.id.nav_settings:
+
+                message = "Settings";
+                break;
+
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
